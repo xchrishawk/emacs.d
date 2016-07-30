a510aff30c52c0ab5fa55831a6a4ce729457ffed
Chris Vig <chris@invictus.so>
2016-07-29 20:16:52 -0500

    Update how backup files are created.

--- init.el
+++ init.el
@@ -5,3 +5,13 @@
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
+
+;; Set up backup file creation
+;; http://stackoverflow.com/questions/151945/how-do-i-control-how-emacs-makes-backup-files
+(setq backup-directory-alist `(("." . "~/.emacs.backup")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
