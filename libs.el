878c823ff6ebcc6f6c6dcc8aa81ac994ca177aff
Chris Vig <chris@invictus.so>
2016-07-29 21:49:39 -0500

    Enable MELPA.

--- libs.el
+++ libs.el
@@ -1,6 +1,12 @@
 ;; libs.el
 ;; Chris Vig (chris@invictus.so)
 
+;; Enable MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/")
+             t)
+
 ;; Enable IDO mode
 (require 'ido)
 (ido-mode t)
